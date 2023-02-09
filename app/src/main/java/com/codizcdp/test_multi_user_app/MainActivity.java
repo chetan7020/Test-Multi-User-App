15498956a7653013244ba722f8e661da01ba2ad4
TMR <chetandagajipatil333@gmail.com>
2023-02-09 15:20:49 +0530

    firebase code added

--- app/src/main/java/com/codizcdp/test_multi_user_app/MainActivity.java
+++ app/src/main/java/com/codizcdp/test_multi_user_app/MainActivity.java
@@ -1,14 +1,53 @@
 package com.codizcdp.test_multi_user_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore firebaseFirestore;
+    private static final String TAG = "MainActivity";
+
+    private void init(){
+        firebaseFirestore = FirebaseFirestore.getInstance();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Alan");
+        user.put("middle", "Mathison");
+        user.put("last", "Turing");
+        user.put("born", 1912);
+
+        firebaseFirestore.collection("Test")
+                .document()
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, "onSuccess: Data Added");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: "+e.getMessage());
+                    }
+                });
     }
 }
